efef394ea399c319c6c4cc7175e0bbbd251143ec
Uwe L. Korn <uwe.korn@quantco.com>
2022-10-25 09:42:35 +0200

    Linux to rt on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [[ "${target_platform}" == "linux-64" ]]; then
+  export CFLAGS="${CFLAGS} -lrt"
+  export CXXFLAGS="${CXXFLAGS} -lrt"
+fi
+
 ./autogen.sh
 ./configure --prefix=$PREFIX
 make -j${CPU_COUNT}
